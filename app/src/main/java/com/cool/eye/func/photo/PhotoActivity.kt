9bfdfc7e3e1373ea384887f187c0a0cc2a3dfac3
ycb <yangchuanbo@sudiyi.cn>
2019-12-03 13:06:21 +0800

    测试拍照1.4.0

--- app/src/main/java/com/cool/eye/func/photo/PhotoActivity.kt
+++ app/src/main/java/com/cool/eye/func/photo/PhotoActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.cool.eye.demo.R
+import com.eye.cool.permission.PermissionHelper
 import com.eye.cool.photo.PhotoDialog
 import com.eye.cool.photo.PhotoDialogActivity
 import com.eye.cool.photo.PhotoDialogFragment
 import com.eye.cool.photo.PhotoHelper
 import com.eye.cool.photo.params.ImageParams
+import com.eye.cool.photo.params.Params
 import com.eye.cool.photo.support.OnSelectListener
+import com.eye.cool.photo.support.PermissionInvoker
 import com.eye.cool.photo.utils.ImageUtil
 import kotlinx.android.synthetic.main.activity_photo.*
 
@@ -22,44 +25,56 @@ class PhotoActivity : AppCompatActivity() {
   }
 
   fun selectUsePhotoDialog(v: View) {
-    PhotoDialog.Builder()
-        .setImageParams(
-            ImageParams.Builder()
-                .setOnSelectListener(object : OnSelectListener {
-                  override fun onSelect(path: String) {
-                    loadImage(path) {
-                      resultIv.setImageBitmap(it)
-                    }
-                  }
-                })
-                .build()
-        )
-        .requestCameraPermission(true) //manifest中声明了，必须设置
-        .build()
-        .show(supportFragmentManager)
+    PhotoDialog.create(
+        Params.Builder()
+            .setImageParams(
+                ImageParams.Builder()
+                    .setOnSelectListener(object : OnSelectListener {
+                      override fun onSelect(path: String) {
+                        loadImage(path) {
+                          resultIv.setImageBitmap(it)
+                        }
+                      }
+                    })
+                    .build()
+            )
+            .setPermissionInvoker(object : PermissionInvoker {
+              override fun request(permissions: Array<String>, invoker: (Boolean) -> Unit) {
+                PermissionHelper.Builder(this@PhotoActivity)
+                    .permissions(permissions)
+                    .permissionCallback(invoker)
+                    .showRationaleWhenRequest(true)
+                    .build()
+                    .request()
+              }
+            })
+            .requestCameraPermission(true) //manifest中声明了Camera，必须设置为true
+            .setAuthority("test")
+            .build()
+    ).show(supportFragmentManager)
   }
 
   fun selectUsePhotoDialogFragment(v: View) {
-    PhotoDialogFragment.Builder()
-        .setImageParams(
-            ImageParams.Builder()
-                .setOnSelectListener(object : OnSelectListener {
-                  override fun onSelect(path: String) {
-                    loadImage(path) {
-                      resultIv.setImageBitmap(it)
-                    }
-                  }
-                })
-                .build()
-        )
-        .requestCameraPermission(true) //manifest中声明了Camera，必须设置为true
-        .build()
-        .show(fragmentManager)
+    PhotoDialogFragment.create(
+        ImageParams.Builder()
+            .setCutAble(false)
+            .setOnSelectListener(object : OnSelectListener {
+              override fun onSelect(path: String) {
+                loadImage(path) {
+                  resultIv.setImageBitmap(it)
+                }
+              }
+            })
+            .build(),
+        true,  //manifest中声明了Camera，必须设置为true
+        null
+    ).show(fragmentManager)
   }
 
   fun selectUsePhotoHelper(v: View) {
     val helper = PhotoHelper(this)
     val imageParams = ImageParams.Builder()
+        .setCutAble(false)
         .setOnSelectListener(object : OnSelectListener {
           override fun onSelect(path: String) {
             loadImage(path) {
