37529afcf25f769ba6057ca974d6c73000703fd9
ycb <yangchuanbo@sudiyi.cn>
2019-12-09 18:00:02 +0800

    banner调试

--- app/src/main/java/com/cool/eye/func/banner/BannerActivity.kt
+++ app/src/main/java/com/cool/eye/func/banner/BannerActivity.kt
@@ -1,23 +1,36 @@
 package com.cool.eye.func.banner
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.cool.eye.demo.R
 import com.eye.cool.banner.CarouselParams
 import kotlinx.android.synthetic.main.activity_banner.*
+import kotlin.random.Random
 
 /**
  *Created by ycb on 2019/11/14 0014
  */
 class BannerActivity : AppCompatActivity() {
 
+  private val pages = mutableListOf<Any>(1)
+  private val adapter = BannerPagerAdapter(pages)
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_banner)
-    viewPager.adapter = BannerPagerAdapter()
+    viewPager.adapter = adapter
     indicator.alwaysShownWhenOnlyOne(false)
     viewPager.params = CarouselParams.Builder()
         .setIndicator(indicator)
+        .setScrollAble(true)
+        .setInterval(3000)
+        .setScrollDuration(3000)
+        .setReversible(true)
+        .setRecyclable(false)
+        .setAutoCarousel(false)
+        .enableCarousel(true)
+        .setDirection(CarouselParams.LEFT_TO_RIGHT)
         .setScrollWhenOne(false)
         .build()
   }
@@ -30,5 +43,14 @@ class BannerActivity : AppCompatActivity() {
   override fun onPause() {
     super.onPause()
     viewPager.stop()
+  }
+
+  fun changePagerNum(v: View) {
+    pages.clear()
+    val size = Random.nextInt(5)
+    (0..size).forEach {
+      pages.add(it)
+    }
+    adapter.notifyDataSetChanged()
   }
 }
--- app/src/main/java/com/cool/eye/func/banner/BannerPagerAdapter.kt
+++ app/src/main/java/com/cool/eye/func/banner/BannerPagerAdapter.kt
@@ -1,30 +1,44 @@
 package com.cool.eye.func.banner
 
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.cool.eye.demo.R
 
 
 /**
  *Created by ycb on 2019/1/8 0008
  */
-class BannerPagerAdapter() : androidx.viewpager.widget.PagerAdapter() {
+class BannerPagerAdapter(private val pages: List<Any>) : androidx.viewpager.widget.PagerAdapter() {
   override fun isViewFromObject(p0: View, p1: Any): Boolean {
     return p0 == p1
   }
 
   override fun getCount(): Int {
-    return 1
+    return pages.size
   }
 
   override fun instantiateItem(container: ViewGroup, position: Int): Any {
+    val fl = FrameLayout(container.context)
+    val tv = TextView(container.context)
     val imageView = ImageView(container.context)
     imageView.layoutParams = ViewGroup.LayoutParams(-1, -1)
     imageView.scaleType = ImageView.ScaleType.CENTER_CROP
     imageView.setImageResource(R.mipmap.ic_launcher)
-    container.addView(imageView)
-    return imageView
+    fl.addView(imageView)
+    fl.addView(tv)
+    tv.textSize = 30f
+    tv.setTextColor(Color.BLACK)
+    tv.text = "Position:$position"
+    container.addView(fl)
+    fl.setOnClickListener {
+      Toast.makeText(container.context, "Click $position", Toast.LENGTH_SHORT).show()
+    }
+    return fl
   }
 
   override fun destroyItem(container: View, position: Int, `object`: Any) {
