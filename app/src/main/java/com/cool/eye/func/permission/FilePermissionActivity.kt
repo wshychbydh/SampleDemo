db940e5df232b05dd87b578b27a011660899bdfb
ycb <yangchuanbo@sudiyi.cn>
2019-12-19 18:08:07 +0800

    ui调整

--- app/src/main/java/com/cool/eye/func/permission/FilePermissionActivity.kt
+++ app/src/main/java/com/cool/eye/func/permission/FilePermissionActivity.kt
@@ -21,13 +21,13 @@ class FilePermissionActivity : AppCompatActivity() {
   fun installApk(view: View) {
     val file = File(
         Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-        , "duty.apk")
+        , "xx.apk")
     val intent = Intent(Intent.ACTION_VIEW)
     // 由于没有在Activity环境下启动Activity,设置下面的标签
     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { //判读版本是否在7.0以上
       val file = File(
-          Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "duty.apk")
+          Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "xx.apk")
       //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
       val apkUri = FileProvider.getUriForFile(this, "test", file)
       intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
