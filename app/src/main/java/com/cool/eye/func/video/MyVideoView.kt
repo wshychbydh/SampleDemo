fd24fb21a06f629185f82556f525634d97511878
ycb <yangchuanbo@sudiyi.cn>
2020-01-20 14:39:59 +0800

    完善视频播放

--- app/src/main/java/com/cool/eye/func/video/MyVideoView.kt
+++ app/src/main/java/com/cool/eye/func/video/MyVideoView.kt
@@ -3,6 +3,7 @@ package com.cool.eye.func.video
 import android.content.pm.ActivityInfo
 import android.graphics.Color
 import android.net.Uri
+import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.Surface
@@ -21,30 +22,44 @@ import kotlinx.android.synthetic.main.video_controller_view.view.*
  *Created by ycb on 2020/1/6 0006
  */
 class MyVideoView @JvmOverloads constructor(
-    private val activity: AppCompatActivity
-) : ConstraintLayout(activity), View.OnClickListener, CompoundButton.OnCheckedChangeListener,
+    private val activity: AppCompatActivity,
+    private val bundle: Bundle
+) : FrameLayout(activity), View.OnClickListener, CompoundButton.OnCheckedChangeListener,
     SeekBar.OnSeekBarChangeListener, Runnable, LifecycleObserver {
 
   private val videoView: VideoView
+  private val videoContainer: ConstraintLayout
+  private val videoDurationTv: TextView
   private val seekBar: SeekBar
   private val videoPauseCb: CheckBox
+  private val videoPlayBtn: ImageButton
 
   private var isPaused = false
+
+  private var isVideoPrepared = false
+
+  private lateinit var duration: String
 
   private val orientationDetector = MyOrientationDetector(activity)
   var rotation = activity.windowManager.defaultDisplay.rotation
 
+  private val portraitHeight = (resources.displayMetrics.density * 210f).toInt()
+
   init {
-    setBackgroundColor(Color.RED)
+    setBackgroundColor(Color.WHITE)
     activity.lifecycle.addObserver(this)
     activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
     activity.window.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     val view = LayoutInflater.from(context).inflate(R.layout.video_controller_view, this, true)
     videoView = view.videoView
-    videoPauseCb = view.videoPauseCb
+    videoContainer = view.videoContainer
+    videoPauseCb = view.videoPlayCb
+    videoPlayBtn = view.videoPlayBtn
+    videoDurationTv = view.videoDurationTv
     seekBar = view.videoSeekBar
     seekBar.setOnSeekBarChangeListener(this)
     view.videoFullScreenIv.setOnClickListener(this)
+    view.videoPlayBtn.setOnClickListener(this)
     videoPauseCb.setOnCheckedChangeListener(this)
     setupVideoView()
     onConfigurationChanged()
@@ -54,21 +69,25 @@ class MyVideoView @JvmOverloads constructor(
     val uri = Uri.parse("android.resource://${activity.packageName}/${R.raw.test}")
     videoView.setVideoURI(uri)
     // videoView.setMediaController(MediaController(activity))
-    // videoView.setVideoURI(Uri.parse("http://valipl.cp31.ott.cibntv.net/6773662aeb835715bdcf34907/03000801005E009DD6EC958003E880C52CFC16-B9E4-4568-8FBC-E56C4F7C5A2F.mp4?ccode=0502&duration=135&expire=18000&psid=7ee287e139e9d0862e7d9c98fedf5747&ups_client_netip=ab582d49&ups_ts=1578292321&ups_userid=null&utid=Sc3SFVp46UECAbaLhmtCHxHJ&vid=XMzg5MjgwMjgxMg&vkey=A221738d06f267b9c7dfd392650f65819"))
+//    videoView.setVideoURI(Uri.parse(bundle.getString(URL)
+//        ?: throw IllegalArgumentException("URL can not be empty.")))
     videoView.setOnPreparedListener {
-      seekBar.progress = 0
+      isVideoPrepared = true
       seekBar.max = it.duration
-      videoPauseCb.isChecked = true
+      duration = formatTime(it.duration)
+      it.seekTo(bundle.getInt(PROGRESS))
+      start()
     }
     videoView.setOnCompletionListener {
-      videoPauseCb.setOnCheckedChangeListener(null)
-      removeCallbacks(this)
-      videoPauseCb.isChecked = false
+      it.seekTo(0)
       seekBar.progress = 0
-      videoPauseCb.setOnCheckedChangeListener(this)
+      videoPauseCb.isChecked = false
+      videoDurationTv.text = "0:00/$duration"
     }
-    videoView.setOnErrorListener { mp, what, extra ->
-      videoPauseCb.visibility = View.GONE
+
+    videoView.setOnErrorListener { mp, _, _ ->
+      mp.stop()
+      mp.release()
       return@setOnErrorListener false
     }
   }
@@ -99,17 +118,21 @@ class MyVideoView @JvmOverloads constructor(
     videoView.stopPlayback()
   }
 
-  private fun onConfigurationChanged() {
+  fun onSaveInstanceState(outState: Bundle) {
+    outState.putInt(PROGRESS, videoView.currentPosition)
+  }
+
+  fun isWideScreen(): Boolean {
     val dm = DisplayMetrics()
     activity.windowManager.defaultDisplay.getMetrics(dm)
-    val isWideScreen = orientationDetector!!.isWideScreen
-    //竖屏
-    layoutParams = if (!isWideScreen) {
-      //竖屏调整播放区域大小，宽全屏，高根据视频分辨率自适应
-      FrameLayout.LayoutParams(dm.widthPixels, -2)
+    return orientationDetector.isWideScreen
+  }
+
+  private fun onConfigurationChanged() {
+    videoContainer.layoutParams = if (!isWideScreen()) {
+      LayoutParams(resources.displayMetrics.widthPixels, portraitHeight)
     } else {
-      //横屏屏调整播放区域大小，宽、高均全屏，播放区域根据视频分辨率自适应
-      FrameLayout.LayoutParams(dm.widthPixels, dm.heightPixels)
+      LayoutParams(resources.displayMetrics.widthPixels, resources.displayMetrics.heightPixels)
     }
   }
 
@@ -124,17 +147,30 @@ class MyVideoView @JvmOverloads constructor(
   }
 
   override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
-    removeCallbacks(this)
     if (isChecked) {
       videoView.start()
       postDelayed(this, 60L)
+      videoPlayBtn.visibility = View.GONE
     } else {
       videoView.pause()
+      videoPlayBtn.visibility = View.VISIBLE
     }
+  }
+
+  fun start() {
+    if (!isVideoPrepared) return
+    videoPb.visibility = View.GONE
+    if (videoView.currentPosition >= videoView.duration) {
+      videoView.seekTo(0)
+    }
+    videoPauseCb.isChecked = true
   }
 
   override fun onClick(v: View?) {
     when (v?.id) {
+      R.id.videoPlayBtn -> {
+        videoPauseCb.isChecked = true
+      }
       R.id.videoFullScreenIv -> {
         val rotation = activity.windowManager.defaultDisplay.rotation
         activity.requestedOrientation = if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
@@ -147,13 +183,20 @@ class MyVideoView @JvmOverloads constructor(
   }
 
   override fun run() {
-    videoView.currentPosition
+    if (!videoView.isPlaying) return
     val pos = videoView.currentPosition
     if (pos != seekBar.progress) {
       seekBar.progress = pos
     }
-    if (videoView.isPlaying) {
+    videoDurationTv.text = String.format("%s/%s", formatTime(pos), duration)
+    //播放sdy截取8秒的流视频的时候，会出现currentPosition大于duration的情况
+    if (videoView.currentPosition < videoView.duration) {
       postDelayed(this, 60L)
+    } else {
+      videoPlayCb.isChecked = false
+      videoView.seekTo(0)
+      seekBar.progress = 0
+      videoDurationTv.text = "0:00/$duration"
     }
   }
 
@@ -163,10 +206,45 @@ class MyVideoView @JvmOverloads constructor(
   }
 
   override fun onStartTrackingTouch(seekBar: SeekBar?) {
-    videoPauseCb.isChecked = false
+    //  videoPauseCb.isChecked = false
   }
 
   override fun onStopTrackingTouch(seekBar: SeekBar?) {
-    videoPauseCb.isChecked = true
+    //   videoPauseCb.isChecked = true
+  }
+
+  companion object {
+
+    private fun formatTime(time: Int): String {
+
+      val second = 1000
+      val minute = 60 * second
+      val hour = 60 * minute
+
+      if (time >= hour) {
+        val hours = time / hour
+        val left = time - hours * hour
+        val minutes = left / minute
+        val seconds = (left - minutes * minute) / second
+        return "${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}"
+      }
+
+      if (time >= minute) {
+        val minutes = time / minute
+        val seconds = (time - minutes * minute) / second
+        return "${formatNumber(minutes)}:${formatNumber(seconds)}"
+      }
+
+      return "00:${formatNumber(time / second)}"
+    }
+
+    private fun formatNumber(number: Int): String {
+      return if (number <= 9) {
+        "0$number"
+      } else "$number"
+    }
+
+    const val PROGRESS = "progress"
+    const val URL = "url"
   }
 }
--- app/src/main/java/com/cool/eye/func/video/VideoActivity.kt
+++ app/src/main/java/com/cool/eye/func/video/VideoActivity.kt
@@ -1,7 +1,11 @@
 package com.cool.eye.func.video
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class VideoActivity : AppCompatActivity() {
@@ -10,8 +14,14 @@ class VideoActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    videoView = MyVideoView(this)
-    setContentView(videoView, ViewGroup.LayoutParams(-1, -2))
+    val bundle = savedInstanceState ?: Bundle()
+    bundle.putString(MyVideoView.URL, intent.getStringExtra(URL))
+    videoView = MyVideoView(this, bundle)
+    if (videoView.isWideScreen()) {
+      requestWindowFeature(Window.FEATURE_NO_TITLE)
+      window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+    setContentView(videoView, ViewGroup.LayoutParams(-1, -1))
   }
 
   override fun onBackPressed() {
@@ -20,115 +30,19 @@ class VideoActivity : AppCompatActivity() {
     }
   }
 
-//  private val runnable = SeekRunnable()
-//  private val seekHandler = Handler()
-//  private lateinit var orientationDetector: MyOrientationDetector
-//
-//  private var isPaused = false
-//
-//  override fun onCreate(savedInstanceState: Bundle?) {
-//    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
-//    window.setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-//    super.onCreate(savedInstanceState)
-//    setContentView(R.layout.activity_video)
-//
-//    setupVideoView()
-//
-//    setSurfaceSize()
-//  }
-//
-//  private fun setupVideoView() {
-//    orientationDetector = MyOrientationDetector(this)
-//    val uri = Uri.parse("android.resource://${packageName}/${R.raw.test}")
-//    videoView.setVideoURI(uri)
-//    videoView.setMediaController(MediaController(this))
-//    // videoView.setVideoURI(Uri.parse("http://valipl.cp31.ott.cibntv.net/6773662aeb835715bdcf34907/03000801005E009DD6EC958003E880C52CFC16-B9E4-4568-8FBC-E56C4F7C5A2F.mp4?ccode=0502&duration=135&expire=18000&psid=7ee287e139e9d0862e7d9c98fedf5747&ups_client_netip=ab582d49&ups_ts=1578292321&ups_userid=null&utid=Sc3SFVp46UECAbaLhmtCHxHJ&vid=XMzg5MjgwMjgxMg&vkey=A221738d06f267b9c7dfd392650f65819"))
-//    videoView.setOnPreparedListener {
-//      println("onPrepared")
-//      it.start()
-//      seekBar.max = it.duration
-//      seekHandler.postDelayed(runnable, 300)
-//    }
-//    videoView.setOnCompletionListener {
-//      println("onComplete")
-//      seekHandler.removeCallbacks(runnable)
-//    }
-//    videoView.setOnErrorListener { mp, what, extra ->
-//      println("onError-->$what ; $extra")
-//      seekHandler.removeCallbacks(runnable)
-//      return@setOnErrorListener false
-//    }
-//    if (Build.VERSION.SDK_INT >= 17) {
-//      videoView.setOnInfoListener { mp, what, extra ->
-//        println("onInfo-->$what ; $extra")
-//        return@setOnInfoListener false
-//      }
-//    }
-//
-//    fullIv.setOnClickListener {
-//      val rotation = windowManager.defaultDisplay.rotation
-//      requestedOrientation = if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
-//        ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-//      } else {
-//        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-//      }
-//    }
-//  }
-//
-//  private inner class SeekRunnable : Runnable {
-//    override fun run() {
-//      seekBar.progress = videoView.currentPosition
-//      seekHandler.postDelayed(this, 300)
-//    }
-//  }
-//
-//  override fun onResume() {
-//    super.onResume()
-//    if (isPaused) {
-//      videoView.start()
-//      isPaused = false
-//    }
-//    orientationDetector.enable()
-//  }
-//
-//  override fun onPause() {
-//    super.onPause()
-//    orientationDetector.disable()
-//  }
-//
-//  override fun onStop() {
-//    super.onStop()
-//    if (videoView.isPlaying) {
-//      isPaused = true
-//    }
-//    videoView.stopPlayback()
-//  }
-//
-//  override fun onConfigurationChanged(newConfig: Configuration) {
-//    super.onConfigurationChanged(newConfig)
-//    setSurfaceSize()
-//  }
-//
-//  private fun setSurfaceSize() {
-//    val dm = DisplayMetrics()
-//    windowManager.defaultDisplay.getMetrics(dm)
-//    val isWideScreen = orientationDetector!!.isWideScreen
-//    //竖屏
-//    if (!isWideScreen) {
-//      //竖屏调整播放区域大小，宽全屏，高根据视频分辨率自适应
-//      videoLayout.layoutParams = FrameLayout.LayoutParams(dm.widthPixels, -2)
-//    } else {
-//      //横屏屏调整播放区域大小，宽、高均全屏，播放区域根据视频分辨率自适应
-//      videoLayout.layoutParams = FrameLayout.LayoutParams(dm.widthPixels, dm.heightPixels)
-//    }
-//  }
-//
-//  override fun onBackPressed() {
-//    val rotation = windowManager.defaultDisplay.rotation
-//    if (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) {
-//      requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-//    } else {
-//      super.onBackPressed()
-//    }
-//  }
+  override fun onSaveInstanceState(outState: Bundle) {
+    videoView.onSaveInstanceState(outState)
+    super.onSaveInstanceState(outState)
+  }
+
+  companion object {
+
+    private const val URL = "url"
+
+    fun launch(context: Context, url: String) {
+      val intent = Intent(context, VideoActivity::class.java)
+      intent.putExtra(URL, url)
+      context.startActivity(intent)
+    }
+  }
 }
